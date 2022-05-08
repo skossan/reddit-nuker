c510dea6a636f27c4d5a582c5a73db1bf39fec05
Rasmus Elmgren <31191132+rasmuselmgren@users.noreply.github.com>
2022-05-08 16:42:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,50 +8,52 @@ def get_username(reddit_object):
 
 def get_all_comments(r):
     username = get_username(r)
-    return r.redditor(str(username)).comments.new()
+    comments = r.redditor(str(username)).comments.new()
+    list_of_comments = []
+    for comment in comments:
+        list_of_comments.append({"comment_id": comment.id,
+                                 "comment_body": comment.body,
+                                 "comment_permalink": comment.permalink})
+    return list_of_comments
 
 
-def delete_all_comments(r, comment_ids):
-    if len(comment_ids) == 0: return print("No comments found")
+def delete_all_comments(r):
+    comments = get_all_comments(r)
 
-    # Will try to delete all comments in list.
-    try:
-        for comment in comment_ids:
-            r.comment(comment).delete()
-    except:
-        print(f"Could not delete comment: {comment_ids}")
-        return
+    if len(comments) == 0: return print("No comments found")
 
-    print(f"Successfully deleted {len(comment_ids)} comments.")
-
-
-def edit_and_delete_all_comments(r, comment_ids):
-    if len(comment_ids) == 0: return print("No comments found")
-
-    # Will try to edit and delete all comments in list
-    for comment in comment_ids:
+    for comment in comments:
         try:
-            r.comment(comment).edit("Lorem ipsum...")
-            r.comment(comment).delete()
+            r.comment(comment["comment_id"]).delete()
         except:
-            print(f"Could not edit and delete comment: {comment}")
-            return
+            print("Could not delete comment: " + comment["comment_id"])
 
-    print(f"Successfully edited and deleted {len(comment_ids)} comments.")
+    print(f"Successfully deleted {len(comments)} comments.")
+
+
+def edit_and_delete_all_comments(r):
+    comments = get_all_comments(r)
+    if len(comments) == 0: return print("No comments found")
+
+    for comment in comments:
+        try:
+            r.comment(comment["comment_id"]).edit("Lorem Ipsum...")
+            r.comment(comment["comment_id"]).delete()
+        except:
+            print("Could not edit and delete comment: " + comment["comment_id"])
+
+    print(f"Successfully edited and deleted {len(comments)} comments.")
 
 
 def show_all_comments(r):
     comments = get_all_comments(r)
-    count = 0
     for comment in comments:
-        count += 1
-        print("------------------")
-        print(f"Comment ID: {comment.id}")
-        print(f"Comment Text: {comment.body}")
-        print(f"Comment Link: https://reddit.com{comment.permalink}")
+        for key, value in comment.items():
+            print(f"{key}: {value}")
+            if key == "comment_permalink":
+                print("------------------")
 
-    print("------------------")
-    print(f"Found {count} comments.")
+    print(f"Found {len(comments)} comments")
 
 
 def main():
@@ -67,8 +69,7 @@ def main():
     if menu_choice == 1 or menu_choice == "1":
         show_all_comments(r)
     elif menu_choice == 2 or menu_choice == "2":
-        comments = []
-        delete_all_comments(r, comments)
+        delete_all_comments(r)
     elif menu_choice == 3 or menu_choice == "3":
         edit_and_delete_all_comments(r)
 
